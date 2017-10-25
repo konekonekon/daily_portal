6e6698fe690957e01c9770b1f90da958f28bb830
Ayano <ayanotarumi@gmail.com>
2017-10-25 16:54:16 +0200

    [server] Problem for refreshing data with 'requests.get'.

--- server.py
+++ server.py
@@ -56,15 +56,15 @@ def index():
 
     #ratp
     #https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&departure_date=25%2F10%2F2017&departure_hour=12&departure_minute=45&op=Rechercher&form_build_id=form-3s9chyTmgWZFUA58gygtM3MiRfgCx1WMrqvDQqAKHfE&form_id=scheduledform
-    page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&op=Rechercher')
+    #page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&op=Rechercher')
     #page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&departure_date=25%2F10%2F2017&departure_hour=13&departure_minute=15&op=Rechercher&form_build_id=form-3s9chyTmgWZFUA58gygtM3MiRfgCx1WMrqvDQqAKHfE&form_id=scheduledform')
-    #page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&op=Rechercher&form_build_id=form-3s9chyTmgWZFUA58gygtM3MiRfgCx1WMrqvDQqAKHfE&form_id=scheduledform')
+    page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&op=Rechercher&form_build_id=form-3s9chyTmgWZFUA58gygtM3MiRfgCx1WMrqvDQqAKHfE&form_id=scheduledform')
     tree = html.fromstring(page.content)
     directions = tree.xpath('//strong[@class="directions"]/text()')
-    passingtime = tree.xpath('//span[@class="heure-wrap"]/text()')
+    heurewrap = tree.xpath('//span[@class="heure-wrap"]/text()')
     # remove the current search time
-    passingtime = [h for h in passingtime if h != passingtime[0]]
-
+    passingtime = [h for h in heurewrap if h != heurewrap[0]]
+    # remove the first item
     if passingtime[0] == "Heure de passage":
         passingtime.remove(passingtime[0])
 
@@ -73,6 +73,7 @@ def index():
             ftime=frtime, jtime=jptime, qtime=qctime,
             # content=get_citiesweatherdata(),
             weatherdata=dict_weatherdata,
+            horaire=heurewrap,
             dirs=directions,
             ptimes=passingtime
             )
