b142147430971a1c965c91ad467fee6aea858e6f
Ayano <ayanotarumi@gmail.com>
2017-10-26 14:45:58 +0200

    [server] Add weather forecast for paris.

--- server.py
+++ server.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 
 def get_francetime():
-    return arrow.now('Europe/Paris').format('dddd, MMMM Do YYYY, H:m')
+    return arrow.now('Europe/Paris').format('dddd, MMMM Do YYYY, HH:mm')
 
 def get_jptime():
-    return arrow.now('Asia/Tokyo').format('dddd, MMMM Do YYYY, H:m')
+    return arrow.now('Asia/Tokyo').format('dddd, MMMM Do YYYY, HH:mm')
 
 def get_quebectime():
-    return arrow.now('Canada/Eastern').format('dddd, MMMM Do YYYY, H:m')
+    return arrow.now('Canada/Eastern').format('dddd, MMMM Do YYYY, HH:mm')
 
 def get_citiesweatherdata():
     weather_key = api_keys['weather']
@@ -42,19 +42,40 @@ def parse_weatherdata(wdata):
         'Temperature':temp, 'Humidity':humidity, 'Wind':wind, 'Clouds':clouds }
 
 
-@app.route('/', methods=['GET'])
-def index():
-    #time
-    frtime = get_francetime()
-    jptime = get_jptime()
-    qctime = get_quebectime()
+def get_weatherdatalist(cid):
+    weather_key = api_keys['weather']
+    res = requests.get(
+        'http://api.openweathermap.org/data/2.5/forecast?id=' + cid + '&units=metric&appid=' + weather_key
+    )
+    weatherforecast_list = json.loads(res.text)['list'][:5]
 
-    #weather
-    dict_weatherdata = list()
-    for databycity in get_citiesweatherdata():
-        dict_weatherdata.append(parse_weatherdata(databycity))
+    return weatherforecast_list
 
-    #ratp
+def parse_weatherdata2(cityid):
+    weatherforecast = {}
+    for w in get_weatherdatalist(cityid):
+        temp = w['main']['temp']
+        humidity = w['main']['humidity']
+        main = w['weather'][0]['main']
+        description = w['weather'][0]['description']
+        icon = w['weather'][0]['icon']
+        wind = w['wind']['speed']
+        clouds = w['clouds']['all']
+        datehours = w['dt_txt']
+        weatherforecast[datehours] = {
+            'temp' : temp,
+            'humidity' : humidity,
+            'weather' : main,
+            'description' : description,
+            'icon' : icon,
+            'wind' : wind,
+            'clouds' : clouds
+        }
+
+    return weatherforecast
+
+
+def get_traininfo():
     #https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&departure_date=25%2F10%2F2017&departure_hour=12&departure_minute=45&op=Rechercher&form_build_id=form-3s9chyTmgWZFUA58gygtM3MiRfgCx1WMrqvDQqAKHfE&form_id=scheduledform
     #page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&op=Rechercher')
     #page = requests.get('https://www.ratp.fr/horaires?networks=rer&line_rer=B&stop_point_rer=Sceaux&type=now&departure_date=25%2F10%2F2017&departure_hour=13&departure_minute=15&op=Rechercher&form_build_id=form-3s9chyTmgWZFUA58gygtM3MiRfgCx1WMrqvDQqAKHfE&form_id=scheduledform')
@@ -68,14 +89,44 @@ def index():
     if passingtime[0] == "Heure de passage":
         passingtime.remove(passingtime[0])
 
+    return passingtime, heurewrap, directions
+
+
+@app.route('/', methods=['GET'])
+def index():
+    #time
+    frtime = get_francetime()
+    jptime = get_jptime()
+    qctime = get_quebectime()
+
+    #weather
+    # City IDs for openweathermap
+    paris_id = '6455259'
+    tokyo_id = '1850147'
+    montreal_id = '6077243'
+
+    #current
+    dict_weatherdata = list()
+    for databycity in get_citiesweatherdata():
+        dict_weatherdata.append(parse_weatherdata(databycity))
+
+    #forecast
+    paris_forecast = parse_weatherdata2(paris_id)
+
+
+    #ratp
+    passtimes, heures, direcs = get_traininfo()
+
 
     return render_template('portal.html',
             ftime=frtime, jtime=jptime, qtime=qctime,
             # content=get_citiesweatherdata(),
             weatherdata=dict_weatherdata,
-            horaire=heurewrap,
-            dirs=directions,
-            ptimes=passingtime
+            p_forecast=paris_forecast,
+            
+            #horaire=heures,
+            dirs=direcs,
+            ptimes=passtimes
             )
 
 
