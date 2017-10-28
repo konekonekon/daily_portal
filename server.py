87196e5f8c1ccff908ec17cdf33498e9b2f6aedc
Ayano <ayanotarumi@gmail.com>
2017-10-28 17:55:29 +0200

    [trains] Put condition to divide trains' passing times for 2 directions in Flask server side.

--- server.py
+++ server.py
@@ -5,44 +5,36 @@ import modules.trains as train
 
 app = Flask(__name__)
 
-
 # @app.route('/weather.png', methods=['GET'])
 # def weather_graph():
 #     response = flask.response(content_type='image/png')
 #     response.body = generate_png_graph()
 #     return response
 
-
 @app.route('/', methods=['GET'])
 def index():
+    cities = ['Paris', 'Tokyo', 'Montreal']
+
     # CURRENT TIME
-    frtime = time.get_francetime()
-    jptime = time.get_jptime()
-    qctime = time.get_quebectime()
+    time_info = {}
+    for city in cities:
+        time_info[city] = time.get_times(city)
 
     # WEATHER FORECAST
-    forecasts = {}
-    for city in ['Paris', 'Tokyo', 'Montreal']:
-        forecasts[city] = weather.get_weatherdata(city)
+    weather_info = {}
+    for city in cities:
+        weather_info[city] = weather.get_weatherdata(city)
 
     # RATP
-    trains = train.get_traininfo()
+    trains_info = train.get_traininfo()
 
 
-    return render_template('portal.html',
-    #         time=time_info,
-    #         weather=weather_info,
-    #         trains=trains_info,
-    # )
-
-            ftime=frtime, jtime=jptime, qtime=qctime,
-
-            wforecasts=forecasts,
-
-            #horaire=heures,
-            trains=trains
-            )
-
+    return render_template(
+        'portal.html',
+        times = time_info,
+        forecasts = weather_info,
+        trains=trains_info
+    )
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0')
